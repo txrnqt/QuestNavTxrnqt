b950eed05d26da228f8484268a76fca8cbfb7439
Juan J Chong <juchong@users.noreply.github.com>
2025-10-08 02:56:16 -0700

    Fix: Use correct namespace for XRGeneralSettingsPerBuildTarget

--- unity/Assets/Editor/CI/Build.cs
+++ unity/Assets/Editor/CI/Build.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
-using UnityEngine.XR.Management;
+using UnityEditor.XR.Management;
 
 namespace CI
 {
